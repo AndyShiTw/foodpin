bcfe2f171d17740f7748e85a21317f087c6fea8e
AndyShiTw <Andy0608@hotmail.com>
2018-03-12 16:40:09 +0800

    新增動畫效果

---
+++ FoodPin/Contorller/ReviewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by HSU JUI-FU on 2018/3/12.
+//  Copyright © 2018年 AndyShi. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
